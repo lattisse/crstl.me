bea7cadf1310209982a16fc4c0f6e08505e76648
lattisse <crystalbajgai@gmail.com>
2025-12-05 19:24:17 +0545

    working blog

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = {
+  blog,
+};
